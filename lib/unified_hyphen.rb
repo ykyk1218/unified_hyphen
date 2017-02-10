ad9f5bbb2ad5bae991529ab00aa559abd9577c04
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-02-10 17:57:36 +0900

    Use constant for hyphen

--- lib/unified_hyphen.rb
+++ lib/unified_hyphen.rb
@@ -2,13 +2,12 @@ require "unified_hyphen/version"
 module UnifiedHyphen
 end
 class String
+  JA_HYPEN_PATTERN = /ー|−|―|-|‐|‑|–|—|ｰ/
   def unify_ja(to_hyphen="-")
-    hyphen = "ー|−|―|-|‐|‑|–|—|ｰ"
-    self.gsub(/#{hyphen}/, to_hyphen)
+    self.gsub(JA_HYPEN_PATTERN, to_hyphen)
   end
 
   def unify_ja!(to_hyphen="-")
-    hyphen = "ー|−|―|-|‐|‑|–|—|ｰ"
-    self.gsub!(/#{hyphen}/, to_hyphen)
+    self.gsub!(JA_HYPEN_PATTERN, to_hyphen)
   end
 end
